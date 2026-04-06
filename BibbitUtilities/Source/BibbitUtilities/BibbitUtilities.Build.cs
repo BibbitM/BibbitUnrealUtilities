85860e6de5da177abe429992a83dc3a91227f9e3
Michał Nowak <bibbit@wp.pl>
2026-04-06 17:04:59 +0200

    Add an empty BibbitUtilities plugin

---
+++ BibbitUtilities/Source/BibbitUtilities/BibbitUtilities.Build.cs
@@ -0,0 +1,21 @@
+// Copyright BIBBIT Michal Nowak.
+
+using UnrealBuildTool;
+
+public class BibbitUtilities : ModuleRules
+{
+	public BibbitUtilities(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange([]);
+
+		PrivateIncludePaths.AddRange([]);
+
+		PublicDependencyModuleNames.AddRange(["Core"]);
+
+		PrivateDependencyModuleNames.AddRange([]);
+
+		DynamicallyLoadedModuleNames.AddRange([]);
+	}
+}
