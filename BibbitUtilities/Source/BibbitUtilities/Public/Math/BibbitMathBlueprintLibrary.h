a6b90cd38d0d9ca1fa5da02f17b700ec0fddfccf
Michał Nowak <bibbit@wp.pl>
2026-06-14 13:38:54 +0200

    Standardize UFUNCTION categories to Bibbit|Math
    Now all the Bibbit functions are located in a separate root category.
    With more functions, it will be easier to find the utility functions.
    The order is the same as for the C++ namespace and the class name.

--- BibbitUtilities/Source/BibbitUtilities/Public/Math/BibbitMathBlueprintLibrary.h
+++ BibbitUtilities/Source/BibbitUtilities/Public/Math/BibbitMathBlueprintLibrary.h
@@ -14,82 +14,82 @@ class BIBBITUTILITIES_API UBibbitMathBlueprintLibrary : public UBlueprintFunctio
 public:
 	/** Calculates the cosine of the angle between A and B.
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector")
 	static double VectorCosine(FVector A, FVector B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the cosine of the angle between A and B.
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Cosine", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector2D")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Cosine", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector2D")
 	static double Vector2DCosine(FVector2D A, FVector2D B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the cosine of the angle between A and B using the XY plane (ignores Z).
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector")
 	static double VectorCosine2D(FVector A, FVector B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the angle between A and B, in radians [0..PI].
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle (Radians)", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle (Radians)", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector")
 	static double VectorAngleInRadians(FVector A, FVector B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the angle between A and B, in degrees [0..180].
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle (Degrees)", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle (Degrees)", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector")
 	static double VectorAngleInDegrees(FVector A, FVector B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the angle between A and B, in radians [0..PI].
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Angle (Radians)", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector2D")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Angle (Radians)", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector2D")
 	static double Vector2DAngleInRadians(FVector2D A, FVector2D B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the angle between A and B, in degrees [0..180].
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Angle (Degrees)", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector2D")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Angle (Degrees)", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector2D")
 	static double Vector2DAngleInDegrees(FVector2D A, FVector2D B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the angle between A and B using the XY plane (ignores Z), in radians [0..PI].
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle 2D (Radians)", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle 2D (Radians)", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector")
 	static double VectorAngle2DInRadians(FVector A, FVector B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the angle between A and B using the XY plane (ignores Z), in degrees [0..180].
 	 * @param EpsilonSq Squared epsilon for vector normalization. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle 2D (Degrees)", AdvancedDisplay = "EpsilonSq"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Angle 2D (Degrees)", AdvancedDisplay = "EpsilonSq"), Category = "Bibbit|Math|Vector")
 	static double VectorAngle2DInDegrees(FVector A, FVector B, double EpsilonSq = 1.e-6);
 
 	/** Calculates the signed angle from A to B around Axis, in radians [-PI..PI]. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle (Radians)", CPP_Default_Axis = "0,0,1"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle (Radians)", CPP_Default_Axis = "0,0,1"), Category = "Bibbit|Math|Vector")
 	static double VectorSignedAngleInRadians(FVector A, FVector B, FVector Axis = FVector::UpVector);
 
 	/** Calculates the signed angle from A to B around Axis, in degrees [-180..180]. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle (Degrees)", CPP_Default_Axis = "0,0,1"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle (Degrees)", CPP_Default_Axis = "0,0,1"), Category = "Bibbit|Math|Vector")
 	static double VectorSignedAngleInDegrees(FVector A, FVector B, FVector Axis = FVector::UpVector);
 
 	/** Calculates the signed angle from A to B in 2D, in radians [-PI..PI]. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Signed Angle (Radians)"), Category = "Math|Bibbit|Vector2D")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Signed Angle (Radians)"), Category = "Bibbit|Math|Vector2D")
 	static double Vector2DSignedAngleInRadians(FVector2D A, FVector2D B);
 
 	/** Calculates the signed angle from A to B in 2D, in degrees [-180..180]. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Signed Angle (Degrees)"), Category = "Math|Bibbit|Vector2D")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Signed Angle (Degrees)"), Category = "Bibbit|Math|Vector2D")
 	static double Vector2DSignedAngleInDegrees(FVector2D A, FVector2D B);
 
 	/** Calculates the signed angle from A to B using the XY plane (ignores Z), in radians [-PI..PI]. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle 2D (Radians)"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle 2D (Radians)"), Category = "Bibbit|Math|Vector")
 	static double VectorSignedAngle2DInRadians(FVector A, FVector B);
 
 	/** Calculates the signed angle from A to B using the XY plane (ignores Z), in degrees [-180..180]. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle 2D (Degrees)"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector Signed Angle 2D (Degrees)"), Category = "Bibbit|Math|Vector")
 	static double VectorSignedAngle2DInDegrees(FVector A, FVector B);
 
 	/** Calculates a vector perpendicular to Vector on the plane defined by Axis. Returns zero if Axis is invalid or parallel. */
-	UFUNCTION(BlueprintPure, meta = (CPP_Default_Axis = "0,0,1"), Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, meta = (CPP_Default_Axis = "0,0,1"), Category = "Bibbit|Math|Vector")
 	static FVector VectorPerpendicular(FVector Vector, FVector Axis = FVector::UpVector);
 
 	/** Calculates a vector perpendicular to Vector using the XY plane (ignores Z). */
-	UFUNCTION(BlueprintPure, Category = "Math|Bibbit|Vector")
+	UFUNCTION(BlueprintPure, Category = "Bibbit|Math|Vector")
 	static FVector VectorPerpendicular2D(FVector Vector);
 
 	/** Calculates a vector perpendicular to Vector. */
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Perpendicular"), Category = "Math|Bibbit|Vector2D")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Vector 2D Perpendicular"), Category = "Bibbit|Math|Vector2D")
 	static FVector2D Vector2DPerpendicular(FVector2D Vector);
 };
